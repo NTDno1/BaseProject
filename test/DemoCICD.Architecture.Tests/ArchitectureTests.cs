67da1167eb56917d53fb30cddb6f5f229552dfb6
DatNT <datt19112001@gmail.com>
2024-12-18 10:42:14 +0700

    first commit

---
+++ test/DemoCICD.Architecture.Tests/ArchitectureTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace DemoCICD.Architecture.Tests;
+public class ArchitectureTests
+{
+    private const string DomainNamespace = "DemoCICD.Domain";
+    private const string ApplicationNamespace = "DemoCICD.Application";
+    private const string InfrastructureNamespace = "DemoCICD.Infrastructure";
+    private const string PersistenceNamespace = "DemoCICD.Persistence";
+    private const string PresentationNamespace = "DemoCICD.Presentation";
+    private const string ApiNamespace = "DemoCICD.API";
+
+    [Fact]
+    public void Domain_Should_Not_HaveDependenceOnOtherProject()
+    {
+        // Arrage
+        var assembly = Domain.AssemblyReference.Assembly;
+
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            PersistenceNamespace,
+            PresentationNamespace,
+            ApiNamespace,
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAny(otherProjects)
+            .GetResult();
+
+        // assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+}
