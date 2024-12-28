41ca915f6a88cb3da23dad28d970100fbaee4420
DatNT <datt19112001@gmail.com>
2024-12-28 09:41:04 +0700

    Update GetProductsQueryHandler.cs

--- src/DemoCICD.Application/UserCases/V1/Queries/Product/GetProductsQueryHandler.cs
+++ src/DemoCICD.Application/UserCases/V1/Queries/Product/GetProductsQueryHandler.cs
@@ -2,7 +2,7 @@
 using MediatR;
 
 namespace DemoCICD.Application.UserCases.V1.Queries.Product;
-public sealed class GetProductsQuery1Handler : IQueryHandler<GetProductQuery, GetProductsResponse>
+public sealed class GetProductsQueryHandler : IQueryHandler<GetProductQuery, GetProductsResponse>
 {
     Task<Result<GetProductsResponse>> IRequestHandler<GetProductQuery, Result<GetProductsResponse>>.Handle(GetProductQuery request, CancellationToken cancellationToken)
     {
